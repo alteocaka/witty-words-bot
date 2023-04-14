f7dbc6c09b9acd26ac7b6b9fd8e7ec1bf5904a2b
Alteo <alteo.caka@gmail.com>
2023-04-14 22:27:32 +0200

    Small fix

--- main.js
+++ main.js
@@ -40,6 +40,6 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running!`);
   job.start();
 });
