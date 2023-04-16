3183efd882cabed4bf0bd34b03a4c491d35ef6c8
Alteo <alteo.caka@gmail.com>
2023-04-16 02:10:19 +0200

    Removed cron job

--- main.js
+++ main.js
@@ -1,27 +1,8 @@
 import client from "./client.js";
 import fetch from "node-fetch";
-import http from 'http';
-import { CronJob } from "cron";
+import http from "http";
 
-// const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
-
-var job = new CronJob("0 */6 * * *", function () {
-  fetch("https://api.quotable.io/random")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      tweet(data);
-      console.log('New tweet!')
-    })
-    .catch((error) => {
-      console.error("There was a problem with the fetch operation:", error);
-    });
-});
 
 const tweet = async (data) => {
   try {
@@ -33,13 +14,26 @@ const tweet = async (data) => {
   }
 };
 
+fetch("https://api.quotable.io/random")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    tweet(data);
+    console.log("New tweet!");
+  })
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
 });
-
-job.start();
 
 server.listen(port, () => {
   console.log(`Server running!`);
